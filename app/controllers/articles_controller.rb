24ef871351307fe5135ebd40d9c42fd6d2c44b31
Merryn <merrynhr@gmail.com>
2021-08-27 17:29:21 +0100

    create method displaying user input

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,10 @@ class ArticlesController < ApplicationController
   def new
 
   end
+
   def create
-    
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to article_path(@article)
   end
 end
