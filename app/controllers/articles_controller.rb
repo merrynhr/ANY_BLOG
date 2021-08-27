0b2cf09221dd6fed35b4afdb8e7e6d3b472d03e8
Merryn <merrynhr@gmail.com>
2021-08-27 17:52:36 +0100

    flash notice added for snew articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
+      flash[:notice] = "New article created!"
       redirect_to article_path(@article)
     else   
       render 'new'
