bfa0cef781f453ea55c50430325b4a2fbb08d1c7
Merryn <merrynhr@gmail.com>
2021-09-01 15:24:30 +0100

    drying up the articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def show; end
 
   def index
     @articles = Article.all
@@ -12,14 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def edit
-    @article = Article.find(params[:id])
-
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article updated!"
       redirect_to @article
     else
@@ -28,7 +24,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "New article created!"
       redirect_to article_path(@article)
@@ -38,8 +34,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
+
+  private 
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
