c33a7a7fc83f8a467707a4a5bce1318ef835fd06
Merryn <merrynhr@gmail.com>
2021-09-09 20:55:28 +0100

    added pagination to articles index page with flickr style css

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
   def show; end
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 10)
   end
 
   def new
