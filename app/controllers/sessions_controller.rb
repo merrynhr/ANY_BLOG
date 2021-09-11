f75bdd11be247b2bbb63859fb7eeaaaf895a653f
Merryn <merrynhr@gmail.com>
2021-09-11 11:24:10 +0100

    log in with sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "Login failed, please try again!"
+      render 'new'
+    end
 
   end
 
   def destroy
     
+
   end
 end
