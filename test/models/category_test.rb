3835b0a1c94cf9953b9e98ceeb8c5cd77d3be828
Merryn <merrynhr@gmail.com>
2021-09-15 17:05:31 +0100

    passing test for name uniqueness validation

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,18 +2,23 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
   
+  def setup
+    @category = Category.new(name: "Music")
+  end
+
   test "category should be valid" do
-    @category = Category.new(name: "Sports")
     assert @category.valid?
   end
 
   test "name should be present" do 
-    @category = Category.new(name: " ")
+    @category.name = " "
     assert_not @category.valid?
   end
 
   test "name should be unique" do
-
+    @category.save
+    @category2 = Category.new(name: "Music")
+    assert_not @category2.valid?
   end
 
   test "name shouldn't be more than 25 chars" do
