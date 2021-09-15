7b710b257d172ce8569241e17a68031d7528a7d8
Merryn <merrynhr@gmail.com>
2021-09-15 16:58:50 +0100

    test passing for name presence validation

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -7,4 +7,21 @@ class CategoryTest < ActiveSupport::TestCase
     assert @category.valid?
   end
 
+  test "name should be present" do 
+    @category = Category.new(name: " ")
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+
+  end
+
+  test "name shouldn't be more than 25 chars" do
+
+  end
+
+  test "name shouldn't be less than 3 chars" do
+
+  end
+
 end
