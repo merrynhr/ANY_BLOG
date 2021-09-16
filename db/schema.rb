ab8c5acc3b792fd62811f4b6e27fb78be30f9fd5
Merryn <merrynhr@gmail.com>
2021-09-16 22:12:35 +0100

    set up many to many relationship between articles and categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_155000) do
+ActiveRecord::Schema.define(version: 2021_09_16_205150) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
