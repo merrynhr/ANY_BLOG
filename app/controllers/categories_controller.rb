de42e4a38cf6e981de27d9e385e404762a5c8c64
Merryn <merrynhr@gmail.com>
2021-09-17 16:59:13 +0100

    Editing categories for admin users now fully functioning with edit view and new form partial

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -27,6 +27,21 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category updated"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+
+  end
+
   private
 
   def category_params
